37ae5cf207379004489fd628ff2e3c45a31fdac0
Carlos Galindo <carlosxgalindo@gmail.com>
2023-03-17 16:56:08 +0000

    Agregando datos

--- juego.sh
+++ juego.sh
@@ -1,3 +1,6 @@
+#Title:BlackjackShell
+#Author:@RokCK
+#Version:0.1.1
 #!/bin/bash
 
 # Funciones
@@ -60,79 +63,133 @@ function crupier_value() {
 }
 
 # Juego
-echo "Blackjack de Carlos"
-
+echo "..:: BLACKJACK ::.."
+sleep 2
+echo ""
+echo -n "Inicianddo"
+sleep 1
+echo -n "."
+sleep 1
+echo -n "."
+sleep 1
+echo -n "."
+echo ""
+sleep 2
 hand=()
 hand+=("$(draw_card)")
 hand+=("$(draw_card)")
 
-echo "Tu mano: ${hand[0]}, ${hand[1]}"
+sleep 1
+echo ""
+echo "-----------"
+echo "Tu mano: ${hand[0]}, ${hand[1]}."
 hand_value=$(hand_value)
+sleep 1
+echo "Tu mano vale $hand_value."
 
 if [[ $hand_value -eq 21 ]]; then
-    echo "21. Ganaste!"
+    sleep 1
+    echo "Felicidades. Ganaste!"
+    echo ""
+    sleep 1
+    echo "Juego terminado."
+    echo "-----------"
     exit 0
 fi
 
 while true; do
+    sleep 1
     read -p "Decide: pides o te quedas? " action
 
     case "$action" in
         p)
             hand+=("$(draw_card)")
-            echo "Sacaste: ${hand[-1]}"
+            sleep 1
+	    echo "Sacaste: ${hand[-1]}."
             hand_value=$(hand_value)
 
             if [[ $hand_value -gt 21 ]]; then
-                echo "Perdiste! Te pasaste."
+		sleep 1
+                echo "Tu mano vale $hand_value. Te pasaste. Has perdido."
+		echo ""
+		sleep 1
+		echo "Juego terminado"
+		echo "-----------"
                 exit 0
             elif [[ $hand_value -eq 21 ]]; then
-                echo "21! Ganaste!"
+		sleep 1
+                echo "Tu mano vale 21! Felicidades. Ganaste."
+		echo ""
+		sleep 1
+		echo "Juego terminado."
+		echo "-----------"
                 exit 0
             else
-                echo "Tu mano vale: $hand_value"
+		sleep 1
+                echo "Tu mano vale: $hand_value."
             fi
             ;;
         q)
+	    sleep 1
             echo "Te quedas con una mano de $hand_value."
             break
             ;;
         *)
+	    sleep 1
             echo "Opcion inexistente. Escribe 'p' o 'q'."
             ;;
     esac
 done
 
+echo ""
+sleep 2
 crupier=()
 crupier+=("$(draw_card)")
 crupier+=("$(draw_card)")
-echo "Crupier: ${crupier[0]}, ${crupier[1]}"
+echo "Crupier: ${crupier[0]}, ${crupier[1]}."
 crupier_value=$(crupier_value)
+sleep 2
+echo "La mano del crupier vale $crupier_value."
 
 if [[ $crupier_hand -eq 21 ]]; then
+    sleep 2
     echo "Crupier saco 21."
     exit 0
 fi
 
 while [[ $crupier_value -lt 17 ]]; do
+        sleep 3
 	crupier+=("$(draw_card)")
-	echo "Crupier saco: ${crupier[-1]}"
+	echo "Crupier saco: ${crupier[-1]}."
 	crupier_value=$(crupier_value)
 
 	if [[ $crupier_value -gt 21 ]]; then
-	    echo "Crupier se paso. Ganaste!"
+	    sleep 2
+	    echo "La mano del crupier vale $crupier_value. Se paso. Ganaste."
+	    echo ""
+	    sleep 1
+	    echo "Juego terminado."
+	    echo "-----------"
 	    exit 0
         elif [[ $crupier_value -eq 21 ]]; then
+	    sleep 2
 	    echo "Crupier saco 21."
 	else
+	    sleep 2
 	    echo "La mano del crupier vale: $crupier_value."
 	fi
 done
 
+echo ""
+sleep 2
 if [[ $hand_value -gt $crupier_value ]]; then
-    echo "Felicidades. Ganaste."
+    echo "$hand_value vence a $crupier_value. Felicidades. Ganaste."
 elif [[ $hand_value -lt $crupier_value ]]; then
-    echo "Has perdido."
+    echo "$crupier_value vence a $hand_value. Lo siento. Has perdido."
 else
-    echo "Es un empate."
+    echo "Ambas manos tienen un valor de $hand_value. Es un empate."
 fi
+echo ""
+sleep 1
+echo "Juego terminado"
+echo "-----------"
